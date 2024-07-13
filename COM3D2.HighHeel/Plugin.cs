3ece26ae3127e695561652ba88677595d355d3ff
InoryS <101243352+InoryS@users.noreply.github.com>
2024-07-14 00:25:33 +0800

    bump version

--- COM3D2.HighHeel/Plugin.cs
+++ COM3D2.HighHeel/Plugin.cs
@@ -14,7 +14,7 @@ namespace COM3D2.HighHeel
     {
         public const string PluginGuid = "com.ongame.com3d2.highheel";
         public const string PluginName = "COM3D2.HighHeel";
-        public const string PluginVersion = "1.0.1";
+        public const string PluginVersion = "1.0.2";
         public const string PluginString = PluginName + " " + PluginVersion;
 
         private const string ConfigName = "Configuration.cfg";
